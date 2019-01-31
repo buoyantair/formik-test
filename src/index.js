add70a8fe0e8052cd1caa69fa5dcad709de7a8e4
buoyantair <buoyantair@protonmail.com>
2019-01-31 15:09:14 +0530

    Add Project
    Finished base line implementation using formik

--- src/index.js
+++ src/index.js
@@ -1,26 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Grommet } from "grommet";
 import "./styles.css";
-const theme = {
-  global: {
-    font: {
-      family: "Roboto",
-      size: "14px",
-      height: "20px"
-    }
-  }
-};
 
-function App() {
-  return (
-    <Grommet theme={theme}>
+import Search from "./search";
+import MoviesList from "./MoviesList";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: []
+    };
+  }
+
+  searchMovies(term) {
+    return new Promise((resolve, reject) => {
+      fetch(`https://www.omdbapi.com/?s=${term}&apikey=f470bab7`)
+        .then(data => data.json())
+        .then(({ Search }) => {
+          this.setState(
+            {
+              results: Search
+            },
+            resolve
+          );
+        })
+        .catch(reject);
+    });
+  }
+
+  render() {
+    return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <header>
+          <h1>Search Movies</h1>
+          <p>
+            Search movies from <i>The Open Movie Database</i>
+          </p>
+        </header>
+        <Search searchMovies={this.searchMovies.bind(this)} />
+        <MoviesList moviesList={this.state.results} />
+        <footer>
+          made by <a href="https://github.com/buoyantair">@buoyantair</a>
+          <br />
+          data from <a href="https://www.omdbapi.com/">OMDb API</a>
+        </footer>
       </div>
-    </Grommet>
-  );
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
