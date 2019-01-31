4b866043ccf248a96858475605ddc3a120be515b
buoyantair <buoyantair@protonmail.com>
2019-01-31 13:43:07 +0530

    Setup Gromet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Grommet } from "grommet";
 import "./styles.css";
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px"
+    }
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Grommet theme={theme}>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </Grommet>
   );
 }
 
